b840f01d05d1a3ca0faa541b50324aa1654e7e57
Natalie Wolfe <natalie@lifewanted.com>
2023-09-29 17:40:03 -0700

    Fix schema to be PSQL compatible.

--- src/schema/version-1.sql
+++ src/schema/version-1.sql
@@ -2,19 +2,19 @@
 CREATE TABLE IF NOT EXISTS locations (
   location_id VARCHAR(32) NOT NULL PRIMARY KEY,
   name        VARCHAR(128) NOT NULL UNIQUE,
-  lat         REAL NOT NULL,
-  lon         REAL NOT NULL,
-  UNIQUE (lat, lon) ON CONFLICT ABORT
+  lat         DOUBLE PRECISION NOT NULL,
+  lon         DOUBLE PRECISION NOT NULL,
+  UNIQUE (lat, lon)
 );
 
 CREATE TABLE IF NOT EXISTS weather_hourly_history (
   location_id   VARCHAR(32) NOT NULL REFERENCES locations (location_id),
-  weather_time  DATETIME NOT NULL,
+  weather_time  TIMESTAMP NOT NULL,
   weather       JSON NOT NULL,
-  temperature   REAL NOT NULL,
-  rain_mm       REAL NOT NULL,
-  snow_mm       REAL NOT NULL,
-  PRIMARY KEY (location_id, weather_time DESC) ON CONFLICT ABORT
+  temperature   DOUBLE PRECISION NOT NULL,
+  rain_mm       DOUBLE PRECISION NOT NULL,
+  snow_mm       DOUBLE PRECISION NOT NULL,
+  PRIMARY KEY (location_id, weather_time)
 );
 
 CREATE INDEX IF NOT EXISTS ordered_weather_hourly_history
